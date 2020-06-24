7534834157583a5ae4064ca3e5b9396d1986e92e
Oskar Korczak <o.korczak@gmail.com>
2020-06-24 20:38:07 +0100

    Switch to PointProgram

--- 3-end-of-the-world/src/main/scala/Interpreter.scala
+++ 3-end-of-the-world/src/main/scala/Interpreter.scala
@@ -2,7 +2,7 @@ object Interpreter {
 
   def main(args: Array[String]): Unit = {
     print(Console.RED)
-    val description: IO[Unit] = PointFreeProgram.createIO(args)
+    val description: IO[Unit] = PointProgram.createDescription(args)
     def interpret[A](description: IO[A]): A = description.unsafeRun()
 
     print(Console.GREEN)
