cb05ee29f370e9b166aaa024bd81209d4bb96da8
Oskar Korczak <o.korczak@gmail.com>
2020-06-24 22:22:22 +0100

    Use IO monads for composition

--- 2-application-library/src/main/scala/PointProgram.scala
+++ 2-application-library/src/main/scala/PointProgram.scala
@@ -9,28 +9,16 @@ object PointProgram {
 
     val firstIO: IO[Unit] = displayKleisli(hyphens)
 
-    val secondIO: IO[Unit] = IO.create {
-      val _: Unit = firstIO.unsafeRun() // first IO result
-
-      val secondIO: IO[Unit] = displayKleisli(question)
-      val secondIOResult: Unit = secondIO.unsafeRun()
-
-      secondIOResult
+    val secondIO: IO[Unit] = M.flatMap(firstIO) { _ =>
+      displayKleisli(question)
     }
 
-    val thirdIO: IO[String] = IO.create {
-      val _: Unit = secondIO.unsafeRun() // second IO result
-
-      val thirdIO: IO[String] = promptKleisli
-      val input: String = thirdIO.unsafeRun()
-
-      input
+    val thirdIO: IO[String] = M.flatMap(secondIO) { _ =>
+      promptKleisli
     }
 
-    val fourthIO: IO[String] = IO.create {
-      val input: String = thirdIO.unsafeRun() // third IO result
-
-      val fourthIO: IO[String] = IO.create {
+    val fourthIO: IO[String] = M.flatMap(thirdIO) { input =>
+      IO.create {
         val integerAmount: Int = converStringToInt(input)
         val positiveAmount: Int = ensureAmountIsPositive(integerAmount)
         val balance: Int = round(positiveAmount)
@@ -38,30 +26,14 @@ object PointProgram {
 
         message
       }
-      val message: String = fourthIO.unsafeRun()
-
-      message
     }
 
-//    val fifthIO: IO[Unit] = M.flatMap(fourthIO)(displayKleisli)
     val fifthIO: IO[Unit] = M.flatMap(fourthIO) { message =>
       displayKleisli(message)
     }
-//    val fifthIO: IO[Unit] = IO.create {
-//      val message: String = fourthIO.unsafeRun() // fourth IO result
-//
-//      val fifthIO: IO[Unit] = displayKleisli(message)
-//      val fifthIOResult: Unit = fifthIO.unsafeRun()
-//
-//      fifthIOResult
-//    }
 
-    val sixthIO: IO[Unit] = IO.create {
-      val _: Unit = fifthIO.unsafeRun() // fifth IO result
-      val sixthIO: IO[Unit] = displayKleisli(hyphens)
-      val sixthIOResult: Unit = sixthIO.unsafeRun()
-
-      sixthIOResult
+    val sixthIO: IO[Unit] = M.flatMap(fifthIO) { _ =>
+      displayKleisli(hyphens)
     }
 
     sixthIO
@@ -72,9 +44,10 @@ object PointProgram {
   private val question: String = "How much money would you like to deposit?"
 
   // side effect (writing to the console)
-  private def displayKleisli(input: Any): IO[Unit] = IO.create {
-    println(input)
-  }
+  private def displayKleisli(input: Any): IO[Unit] =
+    IO.create {
+      println(input)
+    }
 
   private def display(input: Any): Unit = println(input)
 
@@ -86,26 +59,22 @@ object PointProgram {
   // potential side effect (throwing of a NumberFormatException)
   private def converStringToInt(input: String): Int = input.toInt
 
-  private def ensureAmountIsPositive(amount: Int): Int = {
-    if(amount < 1)
+  private def ensureAmountIsPositive(amount: Int): Int =
+    if (amount < 1)
       1
     else
       amount
-  }
 
   @scala.annotation.tailrec
-  private def round(amount: Int): Int = {
-    if(isDivisibleByHundered(amount))
+  private def round(amount: Int): Int =
+    if (isDivisibleByHundered(amount))
       amount
     else
       round(amount + 1)
-  }
 
-  private def isDivisibleByHundered(amount: Int): Boolean = {
+  private def isDivisibleByHundered(amount: Int): Boolean =
     amount % 100 == 0
-  }
 
-  private def createMessage(balance: Int): String = {
+  private def createMessage(balance: Int): String =
     s"Congratulatins, you now have USD $balance."
-  }
 }
